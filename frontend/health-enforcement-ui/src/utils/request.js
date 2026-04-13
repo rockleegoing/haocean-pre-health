73e7f6cd1b5c2b73e89e2dc5adefa91044cd9177
arthur_lee <arthur.going@hotmail.com>
2026-04-13 22:14:48 +0800

    fix: 登录页不弹出会话过期提示

--- frontend/health-enforcement-ui/src/utils/request.js
+++ frontend/health-enforcement-ui/src/utils/request.js
@@ -82,17 +82,20 @@ service.interceptors.response.use(res => {
       return res.data
     }
     if (code === 401) {
-      if (!isRelogin.show) {
-        isRelogin.show = true
-        MessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', { confirmButtonText: '重新登录', cancelButtonText: '取消', type: 'warning' }).then(() => {
-          isRelogin.show = false
-          store.dispatch('LogOut').then(() => {
-            location.href = '/admin/index'
+      // 如果在登录页，不弹出会话过期提示
+      if (window.location.href.indexOf('/login') === -1) {
+        if (!isRelogin.show) {
+          isRelogin.show = true
+          MessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', { confirmButtonText: '重新登录', cancelButtonText: '取消', type: 'warning' }).then(() => {
+            isRelogin.show = false
+            store.dispatch('LogOut').then(() => {
+              location.href = '/admin/index'
+            })
+          }).catch(() => {
+            isRelogin.show = false
           })
-      }).catch(() => {
-        isRelogin.show = false
-      })
-    }
+        }
+      }
       return Promise.reject('无效的会话，或者会话已过期，请重新登录。')
     } else if (code === 500) {
       Message({ message: msg, type: 'error' })
