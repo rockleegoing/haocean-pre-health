43fd005f09065c501e2ced4cb983bb98dd187d16
arthur_lee <arthur.going@hotmail.com>
2026-04-13 22:11:02 +0800

    fix: 修改退出后跳转路径为登录页

--- frontend/health-enforcement-ui/src/layout/components/Navbar.vue
+++ frontend/health-enforcement-ui/src/layout/components/Navbar.vue
@@ -101,7 +101,7 @@ export default {
         type: 'warning'
       }).then(() => {
         this.$store.dispatch('LogOut').then(() => {
-          location.href = '/index'
+          location.href = '/admin/index'
         })
       }).catch(() => {})
     }
